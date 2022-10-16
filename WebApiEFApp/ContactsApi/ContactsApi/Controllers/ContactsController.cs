37fa13f6da7142d33b1aff8450ee2813656088a7
Nick Glavinas <nickosglavinas@gmail.com>
2022-10-16 16:43:31 +0300

    create http request for update and delete

--- WebApiEFApp/ContactsApi/ContactsApi/Controllers/ContactsController.cs
+++ WebApiEFApp/ContactsApi/ContactsApi/Controllers/ContactsController.cs
@@ -20,6 +20,33 @@ namespace ContactsApi.Controllers
          return Ok(await dbContext.Contacts.ToListAsync());
         }
 
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetContact([FromRoute] Guid id)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+               if (contact == null)
+            {
+                return NotFound();
+            }
+               return Ok(contact);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteContact([FromRoute] Guid id)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+            if (contact != null)
+            {
+                dbContext.Remove(contact);
+                await dbContext.SaveChangesAsync();
+                return Ok(contact);
+            }
+            return NotFound();
+
+        }
+
         [HttpPost]
         public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
         {
@@ -36,5 +63,27 @@ namespace ContactsApi.Controllers
             await dbContext.SaveChangesAsync();
             return Ok(contact);
         }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+
+        public async Task<IActionResult> updateContact([FromRoute] Guid id, UpdateContactRequest updateContactRequest)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+            if (contact != null)
+            {
+                contact.FullName = updateContactRequest.FullName;
+                contact.Address = updateContactRequest.Address;
+                contact.Email = updateContactRequest.Email;
+                contact.Phone = updateContactRequest.Phone;
+
+                await dbContext.SaveChangesAsync();
+
+                return Ok(contact);
+            }
+            return NotFound();
+        }
+        
+
     }
 }
---
+++ WebApiEFApp/ContactsApi/ContactsApi/Models/UpdateContactRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactsApi.Models
+{
+    public class UpdateContactRequest
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
